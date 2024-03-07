a1af093b5fffef255d55073a5717a1d1a6a45b76
koxxyPro <augustejean899@gmail.com>
2024-03-07 19:58:40 +0300

    updating accesstoken file

--- accesstoken.php
+++ accesstoken.php
@@ -2,12 +2,12 @@
     define('APP_ENVIROMENT', 'live'); // sandbox or live
     if(APP_ENVIROMENT == 'sandbox'){
         $apiUrl = "https://cybqa.pesapal.com/pesapalv3/api/Auth/RequestToken"; // Sandbox URL
-        $consumerKey = "";
-        $consumerSecret = "";
+        $consumerKey = "ngW+UEcnDhltUc5fxPfrCD987xMh3Lx8";
+        $consumerSecret = "q27RChYs5UkypdcNYKzuUw460Dg=";
     }elseif(APP_ENVIROMENT == 'live'){
         $apiUrl = "https://pay.pesapal.com/v3/api/Auth/RequestToken"; // Live URL
-        $consumerKey = "";
-        $consumerSecret = "";
+        $consumerKey = "buxaS5S0HcDbsw8FOwd1qy7mNcAoF9ba";
+        $consumerSecret = "3b1+wtLWkmayiskwU13pLDvCtWU=";
     }else{
         echo "Invalid APP_ENVIROMENT";
         exit;
@@ -28,6 +28,8 @@
     $response = curl_exec($ch);
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     curl_close($ch);
+
+    //decode to obtain the actual token
     $data = json_decode($response);
 
     $token = $data->token; 
